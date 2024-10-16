9a09878b6911f8ea3c95ae0c39214f9e258c4220
Md. Mahamudul Hasan Tonoy <tonoy@asabd.org>
2024-10-16 15:28:09 +0600

    initial commit

---
+++ Parentheses/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        var stack = new Stack<char>();
+        var parentheses = new Dictionary<char, char> {
+            { ')', '(' },
+            { '}', '{' },
+            { ']', '[' }
+        };
+
+        foreach (char c in s)
+        {
+            if (parentheses.ContainsValue(c))
+            {
+                stack.Push(c);
+            }
+            else if (parentheses.ContainsKey(c))
+            {
+                if (stack.Count == 0 || stack.Pop() != parentheses[c])
+                {
+                    return false;
+                }
+            }
+        }
+        return stack.Count == 0;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var solution = new Solution();
+
+        // Test cases
+        Console.WriteLine(solution.IsValid("()"));         // Output: True
+        Console.WriteLine(solution.IsValid("()[]{}"));     // Output: True
+        Console.WriteLine(solution.IsValid("(]"));         // Output: False
+        Console.WriteLine(solution.IsValid("([])"));       // Output: True
+    }
+}
